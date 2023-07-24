54a6ebc4cf7b6bb68cb9cd17fe83b172ab1a5783
Hans Rivera <hrivera@unal.edu.co>
2023-07-23 20:09:09 -0500

    feat: added query-wrapper to use react-query in every component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,18 @@
 import SearchUsers from "@/components/search-users";
 import UserForm from "@/components/user-form";
 import { SearchParamsProvider } from "@/context/search-params-context";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
-
-const queryClient = new QueryClient();
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <QueryClientProvider client={queryClient}>
-        <SearchParamsProvider>
-          <div className="flex flex-col gap-4">
-            <UserForm />
-            <SearchUsers />
-          </div>
-        </SearchParamsProvider>
-        <Toaster />
-      </QueryClientProvider>
+      <SearchParamsProvider>
+        <div className="flex flex-col gap-4">
+          <UserForm />
+          <SearchUsers />
+        </div>
+      </SearchParamsProvider>
+      <Toaster />
     </main>
   );
 }
